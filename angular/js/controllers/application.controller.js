87612d29ae7de2ff0cb36ff3e7f9ca7ba6bc350f
Martin Lehmann <lehmar11@student.westerdals.no>
2015-03-17 21:43:34 +0100

    Remove unnecessary console.log and params

--- angular/js/controllers/application.controller.js
+++ angular/js/controllers/application.controller.js
@@ -1,11 +1,9 @@
 angular.module('universes').controller('ApplicationController', function ($scope, $location, SessionsService) {
-    $scope.$on('$locationChangeStart', function (event) {
-
+    $scope.$on('$locationChangeStart', function () {
         if ($scope.currentUser) {
             return;
         }
 
-        //if ($location.)
         if ($location.path() === '/login') {
             return;
         }
@@ -14,9 +12,6 @@ angular.module('universes').controller('ApplicationController', function ($scope
     });
 
     $scope.$on('login', function (event, username) {
-        console.log('login');
-        console.log(event);
-        console.log(username);
         $scope.currentUser = username;
     });
 
